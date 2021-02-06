83fa79171fb1b5de6dd13267edee3b1ecf5a15e0
Ubuntu <ubuntu@ip-172-31-47-158.us-east-2.compute.internal>
2021-02-06 22:43:11 +0000

    Adding positivity stuff

--- positivity.js
+++ positivity.js
@@ -1,9 +1,29 @@
-module.exports = class ActivityTracker {
-    constructor(data) {
-        this.data = data
-    }
+var fs = require('fs')
 
-    look(msg) {
-        console.log(msg.content)
-    }
+module.exports = class PositivityTracker {
+	constructor(data) {
+		this.data = data
+		this.positivityObj = JSON.parse(fs.readFileSync("./AFINN.json"))
+	}
+
+	look(msg) {
+		//save message as a string and look through array to calculate positivity score
+		var messageArr = msg.content.toLowerCase().split(" ")
+		var positivityScore = 0;
+
+		//loop through the array that holds the message
+		for(var j = 0; j < messageArr.length; ++j){
+			var messageWord = messageArr[j].replace(/[^a-zA-Z]/ig, "")
+
+			//loop through array that hold positivity scores and see if the keys match the message word
+			for(var h in this.positivityObj){
+				//if the words match sum up the  key values
+				if(messageWord == h){
+					positivityScore += this.positivityObj[h]
+					console.log(positivityScore)
+				}
+			}
+		}
+		console.log("Total Score: " + positivityScore)
+	}
 }
