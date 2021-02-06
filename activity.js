c38cdafcf53920eba7ab67805b66dd54a3ca235d
pizonka662 <pizonka662@gmail.com>
2021-02-06 12:48:29 -0600

    created array

--- activity.js
+++ activity.js
@@ -1,9 +1,13 @@
 module.exports = class ActivityTracker {
     constructor(data) {
         this.data = data
+        this.data.activityData = {}
     }
 
     look(presence) {
         console.log(presence.activities)
+        if (presence.userID in this.data.activityData) {
+
+        }
     }
 }
