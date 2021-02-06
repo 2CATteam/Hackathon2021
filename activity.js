67a156cdff7d19fc91b3c049c2e4db0f79bacee2
pizonka662 <pizonka662@gmail.com>
2021-02-06 13:19:29 -0600

    small fixes

--- activity.js
+++ activity.js
@@ -8,9 +8,8 @@ module.exports = class ActivityTracker {
     look(presence) {
         console.log(presence.activities)
         if (!(presence.userID in this.data.activityData)) {
-            this.data.activityData[presence.userID] = {}
-        }
-        for(var i = 0; i < presence.activities.length - 1; i++)
+            this.data.activityData[presence.userID] = []
+        for(var i = 0; i < presence.activities.length; i++)
         {
             if(presence.activities[i].type == "PLAYING" || presence.activities[i].type == "STREAMING"){
                 this.data.activityData[presence.userID].push({ "start": new Date(), "game": presence.activities[i].name })
