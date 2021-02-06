0841dc8f2f523e0a3292a6255104043b4a968c75
Daniel Royer <2CATteam@gmail.com>
2021-02-06 12:55:15 -0600

    Implemented date chopping

--- activity.js
+++ activity.js
@@ -2,12 +2,28 @@ module.exports = class ActivityTracker {
     constructor(data) {
         this.data = data
         this.data.activityData = {}
+        this.interval = setInterval(this.chop, 10000)
     }
 
     look(presence) {
         console.log(presence.activities)
         if (presence.userID in this.data.activityData) {
 
+        }
+    }
+
+    chop() {
+        var now = new Date()
+        for (var i in this.data.activityData) {
+            for (var j = 0; j < this.data.activityData[i].length; j++) {
+                if (this.data.activityData[i][j].start < now) {
+                    this.data.activityData[i][j].start = now
+                    if (this.data.activityData[i][j].start > this.data.activityData[i][j].end) {
+                        this.data.activityData[i].splice(j, 1)
+                        j--
+                    }
+                }
+            }
         }
     }
 }
